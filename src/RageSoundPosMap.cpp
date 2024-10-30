d7b406989f1a9ba8af622befb03aad051ffaf33e
sukibaby <163092272+sukibaby@users.noreply.github.com>
2024-10-30 06:17:23 -0700

    revert assignment operator change in posmap

--- src/RageSoundPosMap.cpp
+++ src/RageSoundPosMap.cpp
@@ -50,11 +50,9 @@ pos_map_queue::pos_map_queue( const pos_map_queue &cpy )
 
 pos_map_queue &pos_map_queue::operator=( const pos_map_queue &rhs )
 {
-	if (this != &rhs)
-	{
-		pos_map_impl* tempImpl = new pos_map_impl(*rhs.m_pImpl);
-		std::swap(m_pImpl, tempImpl);
-		delete tempImpl;
+	if (this != &rhs){
+		delete m_pImpl;
+		m_pImpl = new pos_map_impl( *rhs.m_pImpl );
 	}
 	return *this;
 }
