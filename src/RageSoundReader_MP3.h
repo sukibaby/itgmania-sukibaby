d765dd56128e952da3235c38c4c109061adf63c3
sukibaby <163092272+sukibaby@users.noreply.github.com>
2024-11-12 05:53:38 -0800

    workin on it!

--- src/RageSoundReader_MP3.h
+++ src/RageSoundReader_MP3.h
@@ -1,58 +1,35 @@
-/* RageSoundReader_MP3 - An interface to read MP3s via MAD. */
-
 #ifndef RAGE_SOUND_READER_MP3_H
 #define RAGE_SOUND_READER_MP3_H
 
 #include "RageSoundReader_FileReader.h"
 #include "RageFile.h"
 
-struct madlib_t;
-
-typedef unsigned long id3_length_t;
-
-signed long id3_tag_query( const unsigned char *data, id3_length_t length );
-void fill_frame_index_cache( madlib_t *mad );
-
 class RageSoundReader_MP3: public RageSoundReader_FileReader
 {
 public:
+
+	/* done */
 	OpenResult Open( RageFileBasic *pFile );
-	void Close();
+	void Close(); // done
+	int Read(float* pBuf, int iFrames);  // done
+	int GetNextSourceFrame() const; // done
+	bool SetProperty(const RString& sProperty, float fValue); // done (?)
+
+	/* still working on it */
 	int GetLength() const { return GetLengthConst(false); }
 	int GetLength_Fast() const { return GetLengthConst(true); }
 	int SetPosition( int iSample );
-	int Read( float *pBuf, int iFrames );
 	unsigned GetNumChannels() const { return Channels; }
 	int GetSampleRate() const { return SampleRate; }
-	int GetNextSourceFrame() const;
-	bool SetProperty( const RString &sProperty, float fValue );
 
+
+	/* ughhhh!  */
 	RageSoundReader_MP3();
 	~RageSoundReader_MP3();
 	RageSoundReader_MP3( const RageSoundReader_MP3 & ); /* not defined; don't use */
 	RageSoundReader_MP3 *Copy() const;
 
 private:
-	int SampleRate;
-	int Channels;
-	bool m_bAccurateSync;
-
-	madlib_t *mad;
-
-
-	bool MADLIB_rewind();
-	int SetPosition_toc( int iSample, bool Xing );
-	int SetPosition_hard( int iSample );
-	int SetPosition_estimate( int iSample );
-
-	int fill_buffer();
-	int do_mad_frame_decode( bool headers_only=false );
-	int resync();
-	void synth_output();
-	int seek_stream_to_byte( int byte );
-	bool handle_first_frame();
-	int GetLengthInternal( bool fast );
-	int GetLengthConst( bool fast ) const;
 };
 
 #endif
